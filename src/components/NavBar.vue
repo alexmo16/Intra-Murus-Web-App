d895b9a65472845f413448bda0d3256c8373231a
Alexis Morel <alexismoreldev@gmail.com>
2018-10-01 22:28:37 -0400

    exemple how to add a bootstrap-vue component in a view

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand >Intra-MurUS</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+import bNavbar from "bootstrap-vue/es/components/navbar/navbar";
+import bNavbarNav from "bootstrap-vue/es/components/navbar/navbar-nav";
+import bNavbarBrand from "bootstrap-vue/es/components/navbar/navbar-brand";
+import bNavbarToggle from "bootstrap-vue/es/components/navbar/navbar-toggle";
+
+export default {
+  name: "NavBar",
+  components: {
+    bNavbar,
+    bNavbarNav,
+    bNavbarBrand,
+    bNavbarToggle
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
+  <div>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"></HelloWorld>
   </div>
 </template>
 
