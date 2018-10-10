137617df68004dd843242386bc75413b9fa75293
Félix Roy <33139437+Rouiller@users.noreply.github.com>
2018-10-09 21:00:41 -0400

    Issue #65 : Bar de navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,36 @@
+<!-- Navigation Bar Layout
+  _____________________________________
+  | Brand                    Dropdown |
+  ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+  Brand : Contains the logo of the website and is linked to the home page.
+  Dropdown - About : Redirect to the about page.
+-->
 <template>
-    <b-navbar toggleable="md" type="dark" variant="info">
+  <b-navbar toggleable="md" type="dark" variant="info">
+    <!-- Brand -->
+    <div>
+      <b-navbar variant="faded" type="light">
+        <b-navbar-brand to="/">
+          <img src="../assets/centre_sportif_logo_brand.png" class="d-inline-block align-center" alt="BV">
+          Intra-MurUS
+        </b-navbar-brand>
+      </b-navbar>
+    </div>
 
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-      <b-navbar-brand to="/">Intra-MurUS</b-navbar-brand>
-
-      <b-collapse is-nav id="nav_collapse">
-
-        <b-navbar-nav>
-          <b-nav-item to="/about">About</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <!-- Using button-content slot -->
-            <template slot="button-content">
-              <em>{{ firstname }}</em>
-            </template>
-            <b-dropdown-item href="#">Signout</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-
-      </b-collapse>
-    </b-navbar>
+    <!-- Right aligned nav items -->
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav class="ml-auto">
+        <!-- Drop down -->
+        <b-nav-item-dropdown right>
+          <template slot="button-content">
+            <em>{{ firstname }}</em>
+          </template>
+          <b-dropdown-item to="/about">About</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+    
+  </b-navbar>
 </template>
 
 <script>
