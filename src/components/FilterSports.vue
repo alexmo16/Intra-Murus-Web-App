978ee2b699d86935f9a4b887e24b7424c2389b4b
Alexis Morel <alexismoreldev@gmail.com>
2018-10-11 11:50:16 -0400

    tuning FilterSports

--- src/components/FilterSports.vue
+++ src/components/FilterSports.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <b-form-select v-model="selectedYear" :options="years" id="year" class="mb-3 combobox"/>
     <b-form-select v-model="selectedSeason" :options="seasons" id="saison" class="mb-3 combobox"/>
     <b-form-select v-model="selectedSport" :options="sports" id="sport" class="mb-3 combobox"/>
     <b-form-select v-model="selectedLeague" :options="leagues" id="ligue" class="mb-3 combobox"/>
@@ -8,10 +9,17 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+@import (reference) "../GlobalStyles.less";
+
 .combobox {
-  max-width: 250px;
+  max-width: 200px;
   min-width: 100px;
   margin-right: 25px;
+  &:focus {
+    border-color: @themDark;
+    box-shadow: 0 0 0.2rem @theme-box-shadow;
+    -webkit-box-shadow: 0 0 0.2rem @theme-box-shadow;
+  }
 }
 </style>
 
@@ -26,17 +34,23 @@ export default {
   data: function() {
     return {
       selectedSport: "Soccer",
-      selectedSeason: "A18",
+      selectedYear: "2018",
+      selectedSeason: "Automne",
       selectedLeague: "AAA",
       sports: [
         { value: "Soccer", text: "Soccer" },
         { value: "Balle molle", text: "Balle molle" },
         { value: "Hockey sur Gazon", text: "Hockey sur Gazon" }
       ],
+      years: [
+        { value: "2016", text: "2016" },
+        { value: "2017", text: "2017" },
+        { value: "2018", text: "2018" }
+      ],
       seasons: [
-        { value: "A18", text: "A18" },
-        { value: "H19", text: "H19" },
-        { value: "E19", text: "E19" }
+        { value: "Automne", text: "Automne" },
+        { value: "Hiver", text: "Hiver" },
+        { value: "Été", text: "Été" }
       ],
       leagues: [
         { value: "AAA", text: "AAA" },
