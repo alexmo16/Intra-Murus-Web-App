7a2f2427c39e8e6e27c987290ca74a91e1f7f22d
Alexis Morel <alexismoreldev@gmail.com>
2018-10-20 15:03:41 -0400

    unit tests fixed

--- tests/unit/TeamApprobationTable.spec.js
+++ tests/unit/TeamApprobationTable.spec.js
@@ -1,12 +1,21 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import TeamApprovationTable from "@/components/TeamApprobationTable.vue";
+import sinon from "sinon";
+import TeamApprobationTable from "@/components/TeamApprobationTable.vue";
 
 describe("TeamApprovationTable.vue", function() {
+  let wrapper;
+  let component;
+
+  before(function() {
+    sinon.stub(TeamApprobationTable, "created").callsFake(function() {});
+
+    wrapper = shallowMount(TeamApprobationTable);
+    component = wrapper.vm;
+  });
+
   describe("resetAttributes()", function() {
     it("should set attributes to their default values", function() {
-      const component = shallowMount(TeamApprovationTable).vm;
-      debugger;
       component.isAcceptClicked = true;
       component.isRefuseClicked = true;
       component.selectedRow = {
