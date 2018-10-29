82efa70fe4409bf389ca2fae3eaed51cb57a7c15
Alexis Morel <alexismoreldev@gmail.com>
2018-10-29 14:33:32 -0400

    unit tests added

--- tests/unit/FilterSports.spec.js
+++ tests/unit/FilterSports.spec.js
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
 import FilterSports from "@/components/FilterSports.vue";
+import sinon from "sinon";
 
 describe("FilterSports.vue", function() {
   let wrapper;
@@ -15,7 +16,7 @@ describe("FilterSports.vue", function() {
       expect(component.filters).to.deep.equals(expectFilters);
     });
 
-    it("should take default values if localStorage is empty.", function() {
+    it("should take default values if not specified", function() {
       wrapper = shallowMount(FilterSports);
       component = wrapper.vm;
 
@@ -23,6 +24,17 @@ describe("FilterSports.vue", function() {
       expect(component.selectedYear).equals(new Date().getFullYear());
       expect(component.selectedLeague).to.be.empty;
       expect(component.selectedSeason).equals("AUTOMNE");
+    });
+
+    it("should call getSportsValues and getLeaguesValues once.", function() {
+      let getSportSpy = sinon.spy(component, "getSportsValues");
+      let getLeaguesSpy = sinon.spy(component, "getLeaguesValues");
+
+      wrapper = shallowMount(FilterSports);
+      component = wrapper.vm;
+
+      expect(getSportSpy.calledOnce);
+      expect(getLeaguesSpy.calledOnce);
     });
   });
 
@@ -34,6 +46,16 @@ describe("FilterSports.vue", function() {
       component.selectedSport = "";
       component.$nextTick(() => {
         expect(component.selectedLeague).to.be.empty;
+      });
+    });
+
+    it("should reset selectedSport on season changed.", function() {
+      wrapper = shallowMount(FilterSports);
+      component = wrapper.vm;
+
+      component.selectedSeason = "";
+      component.$nextTick(() => {
+        expect(component.selectedSport).to.be.empty;
       });
     });
   });
