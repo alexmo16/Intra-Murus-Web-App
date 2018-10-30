d6119c5c9d85ee9bad2bcc1b39e1c6718fa0c1e8
Alexis Morel <alexismoreldev@gmail.com>
2018-10-30 15:03:44 -0400

    change week UI

--- src/components/ScheduleCalendar.vue
+++ src/components/ScheduleCalendar.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="ScheduleCalendar">
+    <div id="calendarHeader">
+      <div class="lastWeekContainer">
+        <img id="lastWeek" src="../assets/down-arrow.svg" @click.stop="changeWeek($event)"/>
+      </div>
+      <div id="weekLabelContainer">
+        <span>Semaine du </span>
+        <span id="weekFirstDay">{{ weekLabel }}</span>
+      </div>
+      <div class="nextWeekContainer">
+        <img id="nextWeek" src="../assets/down-arrow.svg" @click.stop="changeWeek($event)"/>
+      </div>
+    </div>
+    <div id="grid"></div>
   </div>
 </template>
 
@@ -9,8 +22,54 @@
 .ScheduleCalendar {
   width: 75%;
   height: 45px;
-  background-color: #dfff;
   margin: 0 auto;
+  color: @secondary;
+  @buttonswidth: 25px;
+
+  #calendarHeader {
+    width: 100%;
+    display: flex;
+
+    * {
+      margin: 0 auto;
+    }
+
+    #weekLabelContainer {
+      vertical-align: middle;
+      text-align: center;
+    }
+
+    .nextWeekContainer {
+      margin-left: -@buttonswidth - 15px;
+    }
+    .lastWeekContainer {
+      margin-right: -@buttonswidth - 15px;
+    }
+
+    .nextWeekContainer,
+    .lastWeekContainer {
+      width: @buttonswidth;
+      vertical-align: middle;
+      #nextWeek,
+      #lastWeek {
+        vertical-align: middle;
+        width: @buttonswidth;
+        height: @buttonswidth;
+        cursor: pointer;
+      }
+      #nextWeek {
+        transform: rotate(-90deg);
+      }
+
+      #lastWeek {
+        transform: rotate(90deg);
+      }
+    }
+  }
+
+  #grid {
+    width: 100%;
+  }
 }
 </style>
 
@@ -18,14 +77,60 @@
 export default {
   name: "ScheduleCalendar",
 
-  props: {},
-
-  data: function() {
-    return {};
+  props: {
+    months: {
+      required: false,
+      default: function() {
+        return [
+          "Janvier",
+          "Fevrier",
+          "Mars",
+          "Avril",
+          "Mai",
+          "Juin",
+          "Juillet",
+          "Aout",
+          "Septembre",
+          "Octobre",
+          "Novembre",
+          "Decembre"
+        ];
+      }
+    }
   },
 
-  components: {},
+  data: function() {
+    return {
+      weekLabel: "",
+      currentDate: new Date()
+    };
+  },
 
-  created: function() {}
+  created: function() {
+    this.changeWeekLabel();
+  },
+
+  methods: {
+    getSunday: function(d) {
+      let currentDate = new Date(d);
+      let day = currentDate.getDay();
+      let diff = currentDate.getDate() - day;
+      let sundayDate = new Date(currentDate.setDate(diff));
+      return sundayDate;
+    },
+
+    changeWeekLabel: function() {
+      let sunday = this.getSunday(this.currentDate);
+      this.weekLabel = `${sunday.getDate()} ${
+        this.months[sunday.getMonth()]
+      } ${this.currentDate.getFullYear()}`;
+    },
+
+    changeWeek: function(event) {
+      let incrementer = event.target.id === "nextWeek" ? 7 : -7;
+      this.currentDate.setDate(this.currentDate.getDate() + incrementer);
+      this.changeWeekLabel();
+    }
+  }
 };
 </script>
