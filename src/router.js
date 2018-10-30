ec4107ab18c2c8b71383bbb87047636e33fd8fe6
Alexis Morel <alexismoreldev@gmail.com>
2018-10-30 12:01:05 -0400

    store the token in the localstorage instead of cookies.

--- src/router.js
+++ src/router.js
@@ -83,10 +83,26 @@ let __redirectLogin = function(backurl, next) {
 ** next: function must be called to resolve the hook, used for redirect/error/move on next
 */
 router.beforeEach((to, from, next) => {
-  let isJwt = utils.verifyCookie("jwt");
-  if (!isJwt && to.path != "/internal-error") {
+  let isJwtInQuery = false;
+  let nextUrl;
+  let isJwt = !!window.localStorage.getItem("jwt");
+  if (!isJwt) {
+    isJwtInQuery = to.query.jwt ? !!to.query.jwt : null;
+    if (isJwtInQuery) {
+      window.localStorage.setItem("jwt", to.query.jwt);
+      delete to.query.jwt;
+      nextUrl = to.fullPath.split("?")[0];
+    }
+  }
+
+  if (!isJwt && !isJwtInQuery && to.path != "/internal-error") {
     __redirectLogin("http%3a%2f%2flocalhost:8081%2f", next);
   } else {
+    if (nextUrl) {
+      next(nextUrl);
+      return;
+    }
+
     next();
   }
 });
@@ -94,7 +110,7 @@ router.beforeEach((to, from, next) => {
 axios.interceptors.request.use(
   function(config) {
     if (config && utils.isApiCall(config.url)) {
-      let jwt = utils.getCookie("jwt");
+      let jwt = window.localStorage.getItem("jwt");
       if (jwt) {
         config.headers.jwt = jwt;
       }
@@ -116,6 +132,7 @@ axios.interceptors.response.use(
   function(error) {
     if (error.response && error.response.status == 403) {
       utils.deleteAllCookies();
+      window.localStorage.removeItem("jwt");
       __redirectLogin("http%3a%2f%2flocalhost:8081%2f", function() {});
     } else if (error.response && error.response.status == 500) {
       router.push("/internal-error");
