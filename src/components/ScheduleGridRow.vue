bf77436431ed25640ed0f5632e58066eca92a0f0
Félix Roy <33139437+Rouiller@users.noreply.github.com>
2018-11-01 19:17:53 -0400

    Issue #96 : itération 1 de la grille horaire

---
+++ src/components/ScheduleGridRow.vue
@@ -0,0 +1,55 @@
+<template>
+  <tbody class="container">
+    <tr>
+      <td id="hour" rowspan="2"><span>{{hour}}</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr> 
+  </tbody>
+</template>
+
+<style scoped lang="less">
+@import (reference) "../GlobalStyles.less";
+
+.container {
+  padding: 10px;
+}
+
+#hour {
+  height: 50px;
+}
+
+td {
+  border: 2px solid @secondaryLight;
+}
+</style>
+
+
+
+<script>
+export default {
+  name: "schedulegridrow",
+
+  props: {
+    hour: {
+      type: String,
+      required: true,
+      default: ""
+    }
+  }
+};
+</script>
