acaeacf30a6ebd8ab05dacf0fbd810b9d6a0e61b
Alexis Morel <alexismoreldev@gmail.com>
2018-11-19 11:18:17 -0500

    use tui-calendar

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,21 +1,78 @@
 <template>
   <div id="appSchedule">
-    <schedule-calendar></schedule-calendar>
+    <vue-tuicalendar
+    ref="calendar"
+    :options="options"
+    :schedules="schedules"
+    @after-render-schedule="handler"
+    @before-render-schedule="handler"
+    @click-schedule="handler">
+  </vue-tuicalendar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import ScheduleCalendar from "@/components/ScheduleCalendar.vue";
+// @ is an alias to /src\
+import { VueTuicalendar } from "@lkmadushan/vue-tuicalendar";
+import "tui-calendar/dist/tui-calendar.min.css";
 
 export default {
   name: "Schedule",
-  data: function() {
-    return {};
-  },
+
   components: {
-    ScheduleCalendar
-  }
+    VueTuicalendar
+  },
+
+  data() {
+    return {
+      schedules: [
+        {
+          id: "1",
+          calendarId: "1",
+          title: "Bleu vs Rouge",
+          location: "Centre sportif",
+          attendees: ["roy", "favreau"],
+          category: "time",
+          dueDateClass: "",
+          start: "2018-11-19T17:30:00",
+          end: "2018-11-19T19:30:00",
+          isReadOnly: true
+        }
+      ],
+
+      options: {
+        defaultView: "week",
+        taskView: false,
+        scheduleView: ["time"],
+        week: {
+          daynames: [
+            "Dimanche",
+            "Lundi",
+            "Mardi",
+            "Mercredi",
+            "Jeudi",
+            "Vendredi",
+            "Samedi"
+          ],
+          startDayOfWeek: 0,
+          narrowWeekend: false,
+          hourStart: 15,
+          hourStop: 24
+        }
+      }
+    };
+  },
+
+  created: function() {},
+
+  mounted: function() {
+    this.$refs.calendar.registerEvent("beforeDeleteSchedule", function(event) {
+      // do stuff here
+      console.log(event);
+    });
+  },
+
+  methods: {}
 };
 </script>
 
