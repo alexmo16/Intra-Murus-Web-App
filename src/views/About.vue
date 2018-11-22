2bc406ed0ebd6c846af69010f5c598017b65f9b5
Félix Roy <33139437+Rouiller@users.noreply.github.com>
2018-11-21 23:50:24 -0500

    Fix page about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,30 +2,24 @@
   <div class="about">
     <h3 class="header-text">Équipe de développement Intra-MurUS</h3>
     <div class="body">
-      <p class="team-name">API</p>
-      <p class="team-name">Mobile</p>
-      <p class="team-name">Web</p>
-      <div class ="teammember">
+      <div class ="team">
+        <p class="team-name">API</p>
         <b-img class="img1" rounded="circle" width="100" height="100" :src="require('../assets/zucc.png')" />
         <p>Patrick Beaulieu</p>
-      </div>
-      <div class ="teammember">
-        <b-img class="img3" rounded="circle" width="100" height="100" :src="require('../assets/zucc.png')" />
-        <p>Mathieu Favreau</p>
-      </div>
-      <div class ="teammember">
-        <b-img class="img5" rounded="circle" width="100" height="100" :src="require('../assets/zucc.png')" />
-        <p>Alexis Morel</p>
-      </div>
-      <div class ="teammember">
         <b-img class="img2" rounded="circle" width="100" height="100" :src="require('../assets/zucc.png')" />
         <p>Walid Madaoui</p>
       </div>
-      <div class ="teammember">
+      <div class ="team">
+        <p class="team-name">Mobile</p>
+        <b-img class="img3" rounded="circle" width="100" height="100" :src="require('../assets/zucc.png')" />
+        <p>Mathieu Favreau</p>
         <b-img class="img4" rounded="circle" width="100" height="100" :src="require('../assets/zucc.png')" />
         <p>Yanik Gobeil</p>
       </div>
-      <div class ="teammember">
+      <div class ="team">
+        <p class="team-name">Web</p>
+        <b-img class="img5" rounded="circle" width="100" height="100" :src="require('../assets/zucc.png')" />
+        <p>Alexis Morel</p>
         <b-img class="img6" rounded="circle" width="100" height="100" :src="require('../assets/zucc.png')" />
         <p>Félix Roy</p>
       </div>
